fffd89f8753159287e9be56329d99ec7827fec50
Scriddie <alexander.reisach@gmail.com>
2020-05-19 13:34:36 +0200

    installation

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Go to TextMiningCourse directory
+python3 -m venv tm_env
+source tm_env/bin/activate
+cd ..
+git clone https://github.com/huggingface/neuralcoref.git
+cd neuralcoref
+pip install -r requirements.txt
+pip install -e .
