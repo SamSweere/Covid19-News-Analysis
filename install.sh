9e727b11fe360cc9a49cfa492c9fd425ebc7cfb0
Sam Sweere <samsweere@gmail.com>
2020-05-19 14:43:49 +0200

    added some papers

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 # Go to TextMiningCourse directory
 python3 -m venv tm_env
 source tm_env/bin/activate
-cd ..
 git clone https://github.com/huggingface/neuralcoref.git
 cd neuralcoref
 pip install -r requirements.txt
