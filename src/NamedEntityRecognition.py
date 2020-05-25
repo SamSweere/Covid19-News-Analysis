4a6ca33bcdc046b5684847b526c19100ca0420e0
Scriddie <alexander.reisach@gmail.com>
2020-05-25 17:30:24 +0200

    add visualization to new sentiment pipeline

--- src/NamedEntityRecognition.py
+++ src/NamedEntityRecognition.py
@@ -109,15 +109,6 @@ class NamedEntityRecognizer:
         df_most_common = df_most_common.head(10)
         return df_most_common
 
-    def make_label(self, df_most_common):
-        def combine_sum_and_sent(cum_sum, sentiment):
-            s = cum_sum + " " + sentiment
-            return s
-
-        df_most_common["label"] = df_most_common.apply(lambda x: combine_sum_and_sent(x["cum_sum"], x["sentiment"]), axis=1)
-
-        return df_most_common
-
     def visualize(self, df_most_common, start_date, end_date):
         print("Starting Visualization...\t", str(datetime.now()))
         # viz.animate_NER(df_most_common)
@@ -142,17 +133,16 @@ class NamedEntityRecognizer:
                 text = x.text
                 url = x.label_.split(" ")[0]
                 dbpedia_labels = x.label_.split(" ")[1]
-                # TODO maybe use namedtuple here
                 all_items.append((text, url, dbpedia_labels)) 
                 
-                # TODO perhaps look for a more general solution here?
-                # Idea: find a good compromise between standard NER and DBPedia
-                # Washington case: Trust standard NER for washington GPE
-                if text.lower() == "washington":
+                # resolve common disambiguitions
+                if text.lower() == "washington":  # in doubt, go for location
                     standard_ner_ents = {i.text.lower(): i.label_ for i in article_raw["nlp_resolved"].ents}
                     if "washington" in [i.lower() for i in standard_ner_ents.keys()]:
                         if standard_ner_ents["washington"] == "GPE":
                             dbpedia_labels = "Location_State"
+                elif text.lower() == "trudeau":  # in doubt, go for Justin
+                    dbpedia_labels = "Justin Trudeau"
 
                 if entity_type in dbpedia_labels:
                     # append last bit of url to make sure we don't get duplicates
@@ -179,10 +169,6 @@ class NamedEntityRecognizer:
                 # return tuple()
                 return ("None", 0, "".join(list(deepcopy(article.text))))
 
-            # TODO replace all mentions of that entity with its entity_name
-
-            # items = [x.text for x in article.ents if entity_type in x.label_]
-            # return the entity with max ctimedelta(days=1)ount and resolved text
             mce = max(entity_counts)
             mce_list = list(mce)
             mce_val = entity_counts[mce]
@@ -213,8 +199,6 @@ class NamedEntityRecognizer:
                 elif (rep_idx == mce_idx) and in_a_row:
                     # we are still in surface form
                     start -= 1
-
-            # ner_resolved = ""    
             return (mce, mce_val, "".join(ner_resolved))
 
         df[["most_common_1", "most_common_1_num", "ner_resolved"]] = pd.DataFrame.from_records(
@@ -229,8 +213,6 @@ class NamedEntityRecognizer:
         make sure we keep an absent but known entity around
         to avoid the bar from disappearing in bar chart race 
         """
-        assert set(df_most_common.columns) == set(['publication_date', 'most_common_1', 'most_common_1_num', 'sentiment'])
-
         # identify missing values
         all_entities = set(df_most_common["most_common_1"].unique())
         df_gb = df_most_common.groupby(by=["publication_date"])
@@ -248,7 +230,7 @@ class NamedEntityRecognizer:
         # add missing rows and sort again
         df_missing = pd.DataFrame(rows_to_add, columns=df_most_common.columns)
         print(df_missing.head())
-        df_most_common = pd.concat((df_most_common, df_missing), axis=0).reset_index()
+        df_most_common = pd.concat((df_most_common, df_missing), axis=0).reset_index(drop=True)
         df_most_common.sort_values(by=["publication_date", "most_common_1"], inplace=True)
 
         return df_most_common
@@ -369,21 +351,16 @@ class NamedEntityRecognizer:
         df_pp["c_count"] = 1
         return df_pp
     
-
-
-
     def show_problems(self, article, visualize=False):
         """ 
         show some of our current problems in sentiment analyses 
         @param artprint(tsa.get_sentiment("asdf","asdf"))icle: result of spacy nlp applied to the body of an article
         """
-
         # run as:
         # article = df["nlp"][0]
         # sentence = "On March 26, Johnson revealed he had tested positive and that he had been dealing with symptoms since that date."
         # displacy.serve(nlp(sentence), style='dep', options = {'distance': 120})
         # NER.show_problems(article)
-
         # we need to further resolve entities
         item_labels = [(x.text, x.label_) for x in article.ents if not x.text.isspace()]
         print(item_labels)
@@ -391,7 +368,6 @@ class NamedEntityRecognizer:
         if visualize:
             # dependency parsing
             displacy.serve(article, style='dep', options = {'distance': 120})
-
             # entity recognition
             displacy.serve(article, style='ent')
     
@@ -408,56 +384,35 @@ class NamedEntityRecognizer:
         df.sort_values(by=["publication_date", "cum_sum"], ascending=False, inplace=True)
         return df
 
+    def average_sent_columns(self, df):
+        sent_columns = ["g_sent", "t_sent", "t_tls", "c_sent", "c_tls"]
+        for i in sent_columns:
+            df[i] = df[i] / df["most_common_1_num"]
+        return df
+
+    def prepare_viz(self, df_most_common, with_sentiments=False):
+        if "Unnamed: 0" in df_most_common.columns:
+            df_most_common.drop(columns=["Unnamed: 0"], inplace=True)
+        df_most_common.replace("None", np.NaN, inplace=True)
+        df_most_common.dropna(inplace=True)
+        df_most_common.reset_index(drop=True, inplace=False)
+        df_most_common = NER.fill_entity_gaps(df_most_common)
+        df_most_common = NER.cum_sum_df(df_most_common)
+        if with_sentiments:
+            df_most_common = NER.average_sent_columns(df_most_common)
+        df_most_common = NER.select_most_common_per_period(df_most_common)
+        return df_most_common
 
 
-    def debug(self, df_pp):
-        # Functionality for checking a given entity
-        print(df_pp)
-        for i in range(df_pp.shape[0]):
-            if ("Washington" in df_pp.loc[i, :].nlp.text):
-                print(i)
-
-            # show entities of a given article
-            example = df_pp.loc[16, :]
-            print(example.nlp.text)
-            # standard NER
-            items = [(i.text, i.label_) for i in example.nlp.ents]
-            print(items)
-            print()
-            # dbpedia NER
-            dbpedia_items = [(i.text, i.label_, i.start, i.end) for i in example.nlp_resolved.ents]
-            print(dbpedia_items)
-
-            for i in dbpedia_items:
-                print(i)
-            # query dbpedia with example text
-
-            # check coref greedyness
-            nlp = spacy.load(f"en_core_web_sm")  # "eng_core_web_lg" for better but slower results
-            coref = neuralcoref.NeuralCoref(nlp.vocab, greedyness=0.4)
-            nlp.add_pipe(coref, name='neuralcoref')
-            res = nlp(example.nlp.text)
-            ungreedy_text = res._.coref_resolved
-            print(ungreedy_text)
-
-            # TODO the neuralcoref entities look better than the NER ones, can we use those?
-            base_url = "http://localhost:2222/rest"
-            response = requests.get(f"{base_url}/annotate",
-                headers={'accept': 'application/json'},
-                params={'text': "nursing home residents in one Washington state facility"})
-            data = response.json()
-            print(data)
-            for i in data.get("Resources"):
-                print(str(i)+"\n")
-            # look at result closely to make sure we pick right entity
-
-def run_and_save(start_date, end_date, articles_per_period = None, max_length = None):
+def run_and_save(start_date, end_date, articles_per_period=None, max_length=None, debug=False, with_sentiments=False):
     c_date = start_date
 
     # Name 
     run_name = "s_" + start_date.strftime("%d_%m_%Y") + "_e_" \
         + end_date.strftime("%d_%m_%Y") + "_app_" + str(articles_per_period) \
         + "_ml_" + str(max_length) + "_" + datetime.now().strftime("d_%d_%m_t_%H_%M")
+    if debug:
+        run_name = "debug_run"
 
     folder_path = "data/"+run_name
     if not os.path.isdir(folder_path):
@@ -479,31 +434,35 @@ def run_and_save(start_date, end_date, articles_per_period = None, max_length = 
     while(c_date != end_date + timedelta(days=1)):
         print("Running day:",c_date.strftime("%d_%m_%Y"))
         print("Loading Data...\t", str(datetime.now()))
-        # df = read_data.get_body_df(
-        #     start_date=c_date,
-        #     end_date=c_date,
-        #     articles_per_period=articles_per_period, #700,
-        #     max_length=max_length
-        # )
+        if not debug:
+            df = read_data.get_body_df(
+                start_date=c_date,
+                end_date=c_date,
+                articles_per_period=articles_per_period, #700,
+                max_length=max_length
+            )
 
-        df = pd.DataFrame({
-            "body": ["Deepika has a dog. She loves him. The movie star has always been fond of animals",
-            "The short guy Donald Trump is the worst. He does not know how the world turns.",
-            "The tall guy Donald Trump is the best.",
-            "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811. Public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
-            "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease, there were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
-            "Coronavirus disease 2019 (COVID-19) is an infectious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).[10] It was first identified in December 2019 in Wuhan, China, and has since spread globally, resulting in an ongoing pandemic.[11][12] As of 24 May 2020, more than 5.35 million cases have been reported across 188 countries and territories, resulting in more than 343,000 deaths. More than 2.14 million people have recovered."],
-            
-            "publication_date": ["2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05"]
-        })
+        if debug:
+            df = pd.DataFrame({
+                "body": ["Deepika has a dog. She loves him. The movie star has always been fond of animals",
+                "The short guy Donald Trump is the worst. He does not know how the world turns.",
+                "The tall guy Donald Trump is the best.",
+                "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811. Public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
+                "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease, there were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
+                "Coronavirus disease 2019 (COVID-19) is an infectious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).[10] It was first identified in December 2019 in Wuhan, China, and has since spread globally, resulting in an ongoing pandemic.[11][12] As of 24 May 2020, more than 5.35 million cases have been reported across 188 countries and territories, resulting in more than 343,000 deaths. More than 2.14 million people have recovered."],
+                
+                "publication_date": ["2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05"]
+            })
 
         df = NER.spacy_preprocessing(df, model_size="sm") # model_size="lg")
-        df = NER.get_general_sentiment(df)
+        if with_sentiments:
+            df = NER.get_general_sentiment(df)
         df.drop(columns=["body"], inplace=True) # Drop some columns to make some space
         df = NER.dbpedia_ner(df, model_size="sm") #model_size="lg")
         df.drop(columns=["nlp"], inplace=True)
         df = NER.find_most_common_entities(df, "nlp_resolved", entity_type="Person")  # entity "OfficeHolder" is quite nice, "Person" works as well
-        df = NER.get_target_sentiments(df, model_size="sm")
+        if with_sentiments:
+            df = NER.get_target_sentiments(df, model_size="sm")
 
         # TODO check ner_resolved
         # print("aft ts",df.head())
@@ -522,8 +481,6 @@ def run_and_save(start_date, end_date, articles_per_period = None, max_length = 
     elapsed_time = time.process_time() - start_time
     print("Elapsed time: " + str(round(elapsed_time,2)) + " seconds")
     
-    
-
 
 
 if __name__ == "__main__":
@@ -535,70 +492,15 @@ if __name__ == "__main__":
         os.mkdir("src/logs")
 
     # TODO: dates 2019-11-06 and 2020-01-01 throw errors
-
-    start_date=datetime.strptime("2020-02-01", "%Y-%m-%d")
-    # end_date=datetime.strptime("2020-02-02", "%Y-%m-%d")
-    end_date=datetime.strptime("2020-04-05", "%Y-%m-%d")
-    run_and_save(start_date, end_date, articles_per_period = 1000, max_length = 500)
-
-    """
-    print("Loading Data...\t", str(datetime.now()))
-    start_time = time.process_time()
-    
-
     start_date=datetime.strptime("2020-04-01", "%Y-%m-%d")
-    end_date=datetime.strptime("2020-04-06", "%Y-%m-%d")
-    df = read_data.get_body_df(
-        start_date=start_date,
-        end_date=end_date,
-        articles_per_period=10, #700,
-        max_length=300
-    )
+    end_date=datetime.strptime("2020-04-05", "%Y-%m-%d")
+    run_and_save(start_date, end_date, articles_per_period = 1000, max_length = 500, debug=True)
 
-    # print(df.to_string())
-    # df = df.loc[0, :]
-    # df.body = pd.Series("Deepika loves Deepika.")
-
+    # TODO load all dataframes for one experiment and concat them together
     NER = NamedEntityRecognizer()
-    # might be a lot faster if we merge all articles of a day into one document?
-    # df = NER.load_preloaded()
-
-    # TODO log each visualization in  anew folder and save specification
-
-    # TODO we can't keep saving all the preliminary stages in our data frame, we'll run out of ram
-    # TODO: changed the model size to sm
-    df = NER.spacy_preprocessing(df, model_size="sm") # model_size="lg")
-
-    df = df.drop(columns=["body"]) # Drop some columns to make some space
-
-    df = NER.dbpedia_ner(df, model_size="sm") #model_size="lg")
-    # Cleanup df_pp by removing nlp
-    df = df.drop(columns=["nlp"])
-
-    df = NER.find_most_common_entities(df, "nlp_resolved", entity_type="Person")  # entity "OfficeHolder" is quite nice, "Person" works as well
-
-    df = NER.get_target_sentiments(df, model_size="sm")
-
-    # TODO check ner_resolved
-    # print(df_pp.head())
-
-    df = df[["publication_date", "most_common_1", "most_common_1_num", "sentiment"]]
-    df_most_common = NER.sum_period_most_common_entities(df)
-    df_most_common.to_csv("src/logs/df_most_common"+str(datetime.now())+".csv")
-
-
-
-    # df_most_common = NER.fill_entity_gaps(df_most_common)
-    # df_most_common = NER.cum_sum_df(df_most_common)
-    # df_most_common = NER.select_most_common_per_period(df_most_common)
-    # df_most_common = NER.make_label(df_most_common)
-
-    # df_most_common.to_csv("src/logs/df_most_common"+str(datetime.now())+".csv")
+    df_most_common = pd.read_csv("runs/s_01_02_2020_e_05_04_2020_app_1000_ml_500_d_25_05_t_04_00/31_03_2020.csv", index_col=False)
+    df_most_common = NER.prepare_viz(df_most_common)
     print(df_most_common)
-    # NER.visualize(df_most_common, start_date, end_date)
+    NER.visualize(df_most_common, start_date, end_date)
 
-    elapsed_time = time.process_time() - start_time
-    print("Elapsed time: " + str(round(elapsed_time,2)) + " seconds")
 
-    pass
-    """
