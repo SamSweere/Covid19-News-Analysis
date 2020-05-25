f7046ce0b679ff64de4d05dadfff8b05ff886516
Scriddie <alexander.reisach@gmail.com>
2020-05-25 12:54:41 +0200

    compress file

--- src/NamedEntityRecognition.py
+++ src/NamedEntityRecognition.py
@@ -479,43 +479,30 @@ def run_and_save(start_date, end_date, articles_per_period = None, max_length = 
     while(c_date != end_date + timedelta(days=1)):
         print("Running day:",c_date.strftime("%d_%m_%Y"))
         print("Loading Data...\t", str(datetime.now()))
-        df = read_data.get_body_df(
-            start_date=c_date,
-            end_date=c_date,
-            articles_per_period=articles_per_period, #700,
-            max_length=max_length
-        )
+        # df = read_data.get_body_df(
+        #     start_date=c_date,
+        #     end_date=c_date,
+        #     articles_per_period=articles_per_period, #700,
+        #     max_length=max_length
+        # )
 
-        # df = pd.DataFrame({
-        #     "body": ["Deepika has a dog. She loves him. The movie star has always been fond of animals",
-        #     "The short guy Donald Trump is the worst. He does not know how the world turns.",
-        #     "The tall guy Donald Trump is the best.",
-        #     "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811. Public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
-        #     "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease, there were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
-        #     "Coronavirus disease 2019 (COVID-19) is an infectious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).[10] It was first identified in December 2019 in Wuhan, China, and has since spread globally, resulting in an ongoing pandemic.[11][12] As of 24 May 2020, more than 5.35 million cases have been reported across 188 countries and territories, resulting in more than 343,000 deaths. More than 2.14 million people have recovered."],
+        df = pd.DataFrame({
+            "body": ["Deepika has a dog. She loves him. The movie star has always been fond of animals",
+            "The short guy Donald Trump is the worst. He does not know how the world turns.",
+            "The tall guy Donald Trump is the best.",
+            "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811. Public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
+            "There were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease, there were 23 more deaths linked to Covid-19 in the Netherlands, raising the total number of people who died in the country to 5,811 public health agency RIVM said it also knew of another ten hospitalizations for the coronavirus disease.",
+            "Coronavirus disease 2019 (COVID-19) is an infectious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).[10] It was first identified in December 2019 in Wuhan, China, and has since spread globally, resulting in an ongoing pandemic.[11][12] As of 24 May 2020, more than 5.35 million cases have been reported across 188 countries and territories, resulting in more than 343,000 deaths. More than 2.14 million people have recovered."],
             
-        #     "publication_date": ["2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05"]
-        # })
-
-        # print("pre pre",df.head())
+            "publication_date": ["2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05","2020-04-05"]
+        })
 
         df = NER.spacy_preprocessing(df, model_size="sm") # model_size="lg")
-
-        # Get the general sentiment
         df = NER.get_general_sentiment(df)
-
-        df = df.drop(columns=["body"]) # Drop some columns to make some space
-
-        # print("aftg",df.head())
-
+        df.drop(columns=["body"], inplace=True) # Drop some columns to make some space
         df = NER.dbpedia_ner(df, model_size="sm") #model_size="lg")
-        # Cleanup df_pp by removing nlp
-        df = df.drop(columns=["nlp"])
-
+        df.drop(columns=["nlp"], inplace=True)
         df = NER.find_most_common_entities(df, "nlp_resolved", entity_type="Person")  # entity "OfficeHolder" is quite nice, "Person" works as well
-        
-        # print("aft ce, pre ts",df.head())
-
         df = NER.get_target_sentiments(df, model_size="sm")
 
         # TODO check ner_resolved
@@ -527,9 +514,6 @@ def run_and_save(start_date, end_date, articles_per_period = None, max_length = 
 
         file_name = c_date.strftime("%d_%m_%Y")
         df_most_common.to_csv(folder_path + "/" + file_name +".csv")
-
-
-
 
         # Increase the day
         c_date += timedelta(days=1)
