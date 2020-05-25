e7b25eb03b079543a251504cc6ea4ba670c0366c
Sam Sweere <samsweere@gmail.com>
2020-05-25 21:39:37 +0200

    ready for combinded training cluster

--- ABSA-PyTorch/train.py
+++ ABSA-PyTorch/train.py
@@ -218,7 +218,7 @@ def main():
     parser.add_argument('--learning_rate', default=2e-5, type=float, help='try 5e-5, 2e-5 for BERT, 1e-3 for others')
     parser.add_argument('--dropout', default=0.1, type=float)
     parser.add_argument('--l2reg', default=0.01, type=float)
-    parser.add_argument('--num_epoch', default=20, type=int, help='try larger number for non-BERT models')
+    parser.add_argument('--num_epoch', default=10, type=int, help='try larger number for non-BERT models')
     parser.add_argument('--batch_size', default=16, type=int, help='try 16, 32, 64 for BERT models')
     parser.add_argument('--log_step', default=5, type=int)
     parser.add_argument('--embed_dim', default=300, type=int)
